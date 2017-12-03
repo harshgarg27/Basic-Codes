f01b81e313da27a97f4951fa275cd0f699a80fe6
Harsh Sagar Garg <34108113+harshgarg27@users.noreply.github.com>
2017-12-03 06:43:57 +0530

    Add files via upload

--- uniqueNumBitwise.cpp
+++ uniqueNumBitwise.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 int main(){
     int n;
-    cin>>n;
-    int no;
+    int a[]={1,1,2,3,2,3,4};
+
     int ans=0;
     for(int i=0;i<n;i++){
-    cin>>no;
-    ans=ans^no;
+    ans=ans^a[i];
     }
 
     cout<<ans;
